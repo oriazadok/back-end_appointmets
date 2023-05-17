8d0bde7cafeb48f865ae491ac19231afdffde180
oriazadok <oriazadok@gmail.com>
2023-05-17 19:24:46 +0300

    unameAndAppointmentsList

--- routes/api.js
+++ routes/api.js
@@ -34,6 +34,7 @@ router.get("/HairStyleMenu", async (req, res) => {
 });
 
 
+
 // Create the new user account with email and password
 router.post("/signUp", (req, res) => {
   const form = req.body; 
@@ -45,26 +46,92 @@ router.post("/signUp", (req, res) => {
     .then((userRecord) => {
       console.log('Successfully created new user:', userRecord.uid);
       res.json(userRecord);
+
+      const collectionRef = admin.firestore().collection('clients');
+
+      // Create a new document with a unique ID
+      const newDocRef = collectionRef.doc(userRecord.uid);
+      
+      // Add the new document to the Firestore collection
+      newDocRef.set(form)
+      .then(() => {
+        console.log('New document added to Firestore');
+      })
+      .catch((error) => {
+        console.error('Error adding document to Firestore:', error);
+      });
     })
     .catch((error) => {
       console.error('Error creating new user:', error);
-      res.json(undefined);
+      res.json(null);
     });
 
-  const collectionRef = admin.firestore().collection('clients');
+});
 
-  // Create a new document with a unique ID
-  const newDocRef = collectionRef.doc();
-  
-  // Add the new document to the Firestore collection
-  newDocRef.set(form)
-  .then(() => {
-      console.log('New document added to Firestore');
-  })
-  .catch((error) => {
-      console.error('Error adding document to Firestore:', error);
-  });
+// router.post('/signIn', async (req, res) => {
+//   console("dddddd", req.body);
+//   const idToken = req.body.idToken;
 
+//   try {
+//     const decodedToken = await admin.auth().verifyIdToken(idToken);
+//     const uid = decodedToken.uid;
+    
+//     console.log(`Signed in user with UID: ${uid}`);
+//     res.send(decodedToken);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(401).send(error.message);
+//   }
+// });
+
+router.get("/unameAndAppointmentsList", async (req, res) => {
+  const uid = req.query.uid;
+
+  try {
+    const docRef = admin.firestore().collection('clients').doc(uid);
+    docRef.get()
+      .then(doc => {
+        if (doc.exists) {
+          const uname = doc.data().uname;
+          const appointments = doc.data().appointments;
+          res.json({uname, appointments});
+        } else {
+          console.log('Document does not exist');
+        }
+      })
+      .catch(error => {
+        console.log('Error getting document:', error);
+        res.status(500).send('Error getting document');
+      });
+  } catch (error) {
+    console.error('Error updating field:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+router.put("/schedule", async (req, res) => {
+  const dataSchedule = req.body;
+  const dts = dataSchedule.dataToSchedule;
+  const uid = dataSchedule.uid;
+
+  try {
+    const documentRef = admin.firestore().collection('clients').doc(uid);
+
+    // Update the field in the document
+    await documentRef.update({
+      appointments: admin.firestore.FieldValue.arrayUnion(dts)
+    });
+
+    const updatedDoc = await documentRef.get();
+
+    // Extract the appointments field from the document data
+    const appointments = updatedDoc.data().appointments;
+
+    res.status(200).json({ appointments });
+  } catch (error) {
+    console.error('Error updating field:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 
